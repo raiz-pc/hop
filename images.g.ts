a4426c01fce9dd47ab94ea186b4d0c5ee54c4afb
Thepizza-Xbox <156123920+raiz-pc@users.noreply.github.com>
2024-02-22 11:03:11 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,79 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "playerAsset":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f . . . . . . . 
+. . . . f 1 f 1 f f f f . . . . 
+. . . f f 1 1 1 1 1 1 f f . . . 
+. . . . f f f 1 1 f 1 f f . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . f . . . . . . . . 
+. . f f f f f f f f f f f . . . 
+. . f f . . . f . . . . f . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . f . . f f . . . . . 
+. . . . . f f . . . f . . . . . 
+. . . . . f . . . . f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "PlayerWalkLeft":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . f 1 1 1 1 1 f f . . . . . 
+. . . f 1 f 1 f 1 1 f . . . . . 
+. . . f f 1 1 1 1 f f . f . . . 
+. f f . . f f f f f . . f . . . 
+. . f f . . . f . . . f f . . . 
+. . . . f f . f . . f f . . . . 
+. . . . . . f f f f . . . . . . 
+. . . f f . . f f . . . . . . . 
+. . f . f f f f f . . . . . . . 
+. f f . . . . . f . . . f . . . 
+. f f . . . . . . f f f f . . . 
+. . . . . . . . . . f f . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . f 1 1 1 1 1 f f . . . . . 
+. . . f 1 f 1 f 1 1 f . . . . . 
+. . . f f 1 1 1 1 f f . f . . . 
+. f f . . f f f f f . . f . . . 
+. . f f . . . f . . . f f . . . 
+. . . . f f . f . . f f . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . f f f f . . . . . . . 
+. . . . . f . . f f . . . . . . 
+. . . f f f . . . f f f . . . . 
+. . . . . . . . . . . . f . . . 
+. . . . . . . . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
